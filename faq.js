d7882c209fb4ab406353212be1bf7c80afb8e391
Amine Grerifa <aminegrerifa20052@gmail.com>
2025-02-21 15:21:34 +0100

    almost done

--- faq.js
+++ faq.js
@@ -1,15 +1,18 @@
-let display = true ;
-document.querySelectorAll("question").forEach(question => { 
+document.querySelectorAll(".question").forEach(question => {
     question.addEventListener("click", function() {
-        let img = this.querySelector("answer");
-        if (img) {
-            if(display){
-                img.src = 'icon-minus.svg';
-                display = false ;
-            }else{
-                img.src = 'icon-plus.svg';
-                display = true ;
-            }
+        let img = this.querySelector("img");
+        let answer = this.nextElementSibling; 
+
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+            img.src = "icon-plus.svg"; 
+        } else {
+            
+            document.querySelectorAll(".answer").forEach(ans => ans.style.display = "none");
+            document.querySelectorAll(".question img").forEach(icon => icon.src = "icon-plus.svg");
+
+            answer.style.display = "block";
+            img.src = "icon-minus.svg"; 
         }
     });
 });
