4e40e81fe68dda5688cc110b50a2426a9f25aa39
Amine Grerifa <aminegrerifa20052@gmail.com>
2025-02-21 16:21:38 +0100

    Mobile version done, Everything's Done!

--- faq.js
+++ faq.js
@@ -2,29 +2,22 @@ document.querySelectorAll(".question").forEach(question => {
     question.addEventListener("click", function() {
         let img = this.querySelector("img");
         let answer = this.nextElementSibling;
-
         if (answer.classList.contains("active")) {
-            // Start closing animation
             answer.classList.remove("active");
-            answer.style.maxHeight = answer.scrollHeight + "px"; // Set to current height
+            answer.style.maxHeight = answer.scrollHeight + "px"; 
             setTimeout(() => {
                 answer.style.maxHeight = "0";
-            }, 10); // Small delay to trigger the transition
-
+            }, 10); 
             img.src = "icon-plus.svg";
         } else {
-            // Close any open answers first
             document.querySelectorAll(".answer.active").forEach(ans => {
                 ans.classList.remove("active");
-                ans.style.maxHeight = ans.scrollHeight + "px"; // Set to current height
+                ans.style.maxHeight = ans.scrollHeight + "px"; 
                 setTimeout(() => {
                     ans.style.maxHeight = "0";
-                }, 10); // Small delay to trigger the transition
+                }, 10); 
             });
-
             document.querySelectorAll(".question img").forEach(icon => icon.src = "icon-plus.svg");
-
-            // Open selected answer smoothly
             answer.classList.add("active");
             answer.style.maxHeight = answer.scrollHeight + "px";
             img.src = "icon-minus.svg";
